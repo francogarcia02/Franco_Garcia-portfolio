720b38c49c13b635726ca8c7e073c72e85b50187
francogarcia02 <francoalbertogarcia2017@gmail.com>
2023-12-13 01:53:25 -0300

    ultimando detalles

--- js/main.js
+++ js/main.js
@@ -1,5 +1,15 @@
-function main(){
-
+function curriculum() {
+  var link = document.createElement('a');
+  link.href = 'pdfs/FrancoGarcia-Curriculum.pdf';
+  link.download = 'FrancoGarcia-Curriculum.pdf';
+  link.target = '_blank';
+  link.click();
 }
 
-main()
+function curriculum-ingles() {
+  var link = document.createElement('a');
+  link.href = 'pdfs/FrancoGarcia-Curriculum(Ingles).pdf';
+  link.download = 'FrancoGarcia-Curriculum(Ingles).pdf';
+  link.target = '_blank';
+  link.click();
+}
